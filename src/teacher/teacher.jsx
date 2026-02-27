503290a438b2fa06d033bf670c39ad66b87263f7
Bek-ah <rbkhktvns@gmail.com>
2026-02-26 22:25:37 -0700

    Revert
    This reverts commit 2cc4309c38033c70ae1d45c2542c90c2627da0df.

--- src/teacher/teacher.jsx
+++ src/teacher/teacher.jsx
@@ -1,59 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
-import { Submission, SubmissionUpdated } from './submissionNotification.js';
 
-export function Teacher({user, score, teacher}) {
-    const [scores, setScores] = React.useState([]);
-    const [events, setEvent] = React.useState([]);
-    setInterval(() => {
-      const score = Math.floor(Math.random() * 3000);
-      const date = new Date().toLocaleDateString();
-      const userName = 'Jane Doe got a new grade!';
-      this.broadcastSubmission(userName);
-    }, 5000);
-  React.useEffect(() => {
-    const scoresText = localStorage.getItem('score');
-    if (scoresText) {
-      setScores(JSON.parse(scoresText));
-    }
-  }, []);
-  const scoreRows = [];
-  if (scores.length) {
-    for (const [i, scores] of scores.entries()) {
-      scoreRows.push(
-        <tr key={i}>
-          <td>{i}</td>
-          <td>{score.name.split('@')[0]}</td>
-          <td>{score.score}</td>
-          <td>{score.date}</td>
-        </tr>
-      );
-    }
-  } else {
-    scoreRows.push(
-      <tr key='0'>
-        <td colSpan='4'>No students have submitted</td>
-      </tr>
-    );
-  }
-
-  function createMessageArray() {
-    const messageArray = [];
-    for (const [i, event] of events.entries()) {
-      let message = 'unknown';
-      if (event.type === GameEvent.End) {
-        message = `scored ${event.value.score}`;
-      }
-    messageArray.push(
-      <div key={i} className='event'>
-        <span className={'player-event'}>{event.from.split('@')[0]}</span>
-        {message}
-      </div>
-      );
-      }
-      return messageArray;
-    }
-
+export function Teacher() {
   return (
     <main class="teacher-background">
     <h2>Scores</h2>
@@ -66,7 +14,22 @@ export function Teacher({user, score, teacher}) {
             <th>Date</th>
         </tr>
         </thead>
-        <tbody>{scoreRows}</tbody>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>John Smith</td>
+                <td>80%</td>
+                <td>3/1/2026</td>
+            </tr>
+        </tbody>
+            <tbody>
+            <tr>
+                <td>2</td>
+                <td>Jane Doe</td>
+                <td>95%</td>
+                <td>3/2/2026</td>
+            </tr>
+        </tbody>
     </table>
         <button className="login-buttons"><Link className="links" to="../">Home</Link></button>
         <div></div>
@@ -76,11 +39,10 @@ export function Teacher({user, score, teacher}) {
         <div></div>
         <button className="login-buttons"><Link className="links" to="../about">Questions?</Link></button>
     <div></div>
-    <div class="notification">
-        Student
-        <span>{user}</span>
-        <div>{createMessageArray()}</div>
-    </div>
+    <ul class="notification">
+        <li class="student-name">John Smith grade changed</li>
+        <li class="student-name">Jane Doe grade changed</li>
+    </ul>
     </main>
 
   );
